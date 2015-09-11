e1951cd30105d392dc7a12412536b584b2aa226b
unknown <sexylion369@gmail.com>
2015-09-11 18:01:19 +0900

    init

---
+++ src/main/java/com/prographer/demo/springboot/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.prographer.demo.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
+    }
+}
